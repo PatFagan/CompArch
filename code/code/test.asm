76b535d3dfdc9cd6d31f28c923362c2469c5fb0b
PatFagan <46632296+PatFagan@users.noreply.github.com>
2022-09-23 10:46:42 -0400

    class code array

--- code/code/test.asm
+++ code/code/test.asm
@@ -1,30 +1,27 @@
-; Assignment 3.4
-; Order of Operations
+; Program 4.4
+; Array - MASM (64-bit)
+; Copyright (c) 2020 Hall & Slonka
 
-.data	; start of the data program segment, to declare variables
+extrn ExitProcess : proc
 
-identifier BYTE "The answer is: "	; create string
+.data
+array QWORD 1, 2, 3, 4
 
-answer DWORD ?	; create variable of four byte to hold the answer
+.code
+_main PROC
 
-; START MAIN FUNCTION
+; Load using byte offsets
+lea rsi, array 
+mov rax, QWORD PTR [rsi]
+mov rbx, QWORD PTR [rsi + 8]
 
-.code	; program segment that signals the start of code segment
-extern ExitProcess : proc	; creates exit function to be used later
-_main PROC	; start of main function
+; Save using indices
+mov rdx, 2
+mov QWORD PTR [rsi+rdx*8], 10
+mov rdx, 3
+mov QWORD PTR [rsi+rdx*8], 20
 
-mov rax, 7	; assign rax register a value of 7
-mov rbx, 5	; assign rbx register a value of 5
-mov rcx, 3	; assign rcx register a value of 3
-mov rdx, 4	; assign rdx register a value of 4
-
-add rax, rbx	; adding rbx to rax
-add rcx, rdx	; adding rdx to rcx
-sub rax, rcx	; subtract rcx from rax
-; the answer is rax
-
-xor rcx, rcx	; resets value of rcx
-.code	; end of code segment
-call ExitProcess	; calls exit function
-_main ENDP	; end of main function
-END	; end of program
+xor rcx, rcx
+call ExitProcess
+_main ENDP
+END
