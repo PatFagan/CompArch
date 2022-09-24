68d7c3a3ffe4142625ebb55113f552e64bd9d684
PatFagan <46632296+PatFagan@users.noreply.github.com>
2022-09-24 14:37:43 -0400

    finished assignment

--- code/code/test.asm
+++ code/code/test.asm
@@ -1,27 +1,43 @@
-; Program 4.4
-; Array - MASM (64-bit)
-; Copyright (c) 2020 Hall & Slonka
+; Assignment 4.4
+; Differently-Sized Arrays
 
-extrn ExitProcess : proc
+.data	; start of the data program segment, to declare variables
 
-.data
-array QWORD 1, 2, 3, 4
+array WORD 5, 10, 15, 20	; create array of WORDS (2 bytes each element)
+newArray DWORD ?, ?, ?, ?	; create array of DWORDS to fill later (4 bytes each element)
+eight WORD 8
 
-.code
-_main PROC
+.code	; program segment that signals the start of code segment
+extern ExitProcess : proc	; creates exit function to be used later
+_main PROC	; start of main function
 
-; Load using byte offsets
-lea rsi, array 
-mov rax, QWORD PTR [rsi]
-mov rbx, QWORD PTR [rsi + 8]
+lea rsi, array	; load array into rsi register
+lea rdi, newArray	; load newArray into rdi register
 
-; Save using indices
-mov rdx, 2
-mov QWORD PTR [rsi+rdx*8], 10
-mov rdx, 3
-mov QWORD PTR [rsi+rdx*8], 20
+mov rdx, 0	; set rdx to 0 for first element of array
+mov ax, [rsi + rdx * 2]	; mov first value of array into ax
+mul eight	; multiply ax by 8
+mov [rdi + rdx * 4], ax	; move resulting value into newArray
 
-xor rcx, rcx
-call ExitProcess
-_main ENDP
-END
+mov rdx, 1	; set rdx to 1 for first element of array
+mov ax, [rsi + rdx * 2]	; mov first value of array into ax
+mul eight	; multiply ax by 8
+mov [rdi + rdx * 4], ax	; move resulting value into newArray
+
+mov rdx, 2	; set rdx to 2 for first element of array
+mov ax, [rsi + rdx * 2]	; mov first value of array into ax
+mul eight	; multiply ax by 8
+mov [rdi + rdx * 4], ax	; move resulting value into newArray
+
+mov rdx, 3	; set rdx to 4 for first element of array
+mov ax, [rsi + rdx * 2]	; mov first value of array into ax
+mul eight	; multiply ax by 8
+mov [rdi + rdx * 4], ax	; move resulting value into newArray
+
+
+
+xor rcx, rcx	; resets value of rcx
+.code	; end of code segment
+call ExitProcess	; calls exit function
+_main ENDP	; end of main function
+END	; end of program
