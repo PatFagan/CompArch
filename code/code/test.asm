d1f313fe0be3d038e393d40d1b8c97509f34913f
PatFagan <46632296+PatFagan@users.noreply.github.com>
2022-10-19 19:40:41 -0400

    CAPS to lowercase conversion

--- code/code/test.asm
+++ code/code/test.asm
@@ -4,20 +4,25 @@
 .data	; start of the data program segment, to declare variables
 
 src BYTE "I DON'T KNOW WHAT WE'RE YELLING ABOUT!",0	; uppercase version of the string
-lenSrc EQU ($ - src)	; length of the src string
-dst BYTE "",0	; lowercase version
+lensrc EQU ($ - src)	; length of the src string
+dst BYTE ":",0	; lowercase version
 
-; for lenSrc, work through the string, getting caps versions of each letter
-; decrement counter w rep
-
-lea rdi, src	; load src string into rdi
-mov rcx, lenSrc	; set counter to string length
 
 .code	; program segment that signals the start of code segment
 extern ExitProcess : proc	; creates exit function to be used later
 _main PROC	; start of main function
 
-mov rdx, 0	; set rdx to 0 as the index for the first element of the arrays
+
+lea rsi, src	; load src string into rsi
+lea rdi, dst	; load dst string into rdi
+
+add src, 32	; change ascii byte to lowercase equivalent by adding 32 bytes
+
+mov rcx, lensrc	; set counter to string length
+
+cld	; clear direction flag to read left to right
+rep movsd	; move src character to dst string
+
 
 xor rcx, rcx	; resets value of rcx
 .code	; end of code segment
