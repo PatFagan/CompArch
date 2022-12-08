4bdd5de30ba8fe1e78ec2e1e7475fa46da7f4962
PatFagan <46632296+PatFagan@users.noreply.github.com>
2022-12-08 17:39:09 -0500

    cleaned up and finalized most features

--- arduino-script/PatJukebox/PatJukebox.ino
+++ arduino-script/PatJukebox/PatJukebox.ino
@@ -1,27 +1,21 @@
 /*
   Music Box
   using Arduino Esplora
+  by Pat Fagan
+  with help from Brian Hall
 
-  Original musical slider example provided by
+  Original musical slider and microphone examples provided by
   Enrico Gueli & Tom Igoe
+  
 */
 
 #include <Esplora.h>
-
-// add inputs for all buttons
-// add percussion loops
-// use slider to switch chord key 
-// use buttons to play notes in key
-// receive mic input (and add reverb to it)
 
 uint8_t arpeggiatorInput = 1;
 uint8_t arpeggiatorOutput = 3;
 uint8_t arpInc = 1;
 uint8_t arpMax = 4*arpInc;
 uint8_t zero = 0;
-
-uint8_t var1 = 2;
-uint8_t var2 = 0;
 
 // these are the frequencies for the notes from middle C
 // to one octave above middle C:
@@ -46,19 +40,24 @@ void setup() {
 }
 
 void loop() {
+
+  // adjust slider to change tempo (left for slower, right for faster)
   int tempo = Esplora.readSlider();
+  delay(tempo);
   
-  //melody(tempo);
+  // calls the melody function, which uses the joystick to play notes
+  melody();
 
-  //drumLoop();
+  // calls the drum loop function, which uses the top button to play a drum loop
+  drumLoop();
 
-  //getMic(tempo);
+  // calls the get mic function, which plays sound back when it reads sound
+  getMic();
 
-  arpeggiatorInput = arpeggiatorOutput;
+  arpeggiatorInput = arpeggiatorOutput; // ready input data for inline assembly (set to previous output)
 
-  //Serial.print(arpeggiatorInput);
   asm volatile(
-    " arpLoop: "
+    " arpLoop: " // tag for the assembly code section
     " add %[arpeggiatorInput], %[arpInc] \n\t" // increment arpeggiator by arpInc
     " cp %[arpMax], %[arpeggiatorInput] \n\t" // compare arpeggiator with max arpeggiator
     " brsh endLoop \n\t" // branch if same or higher
@@ -67,42 +66,26 @@ void loop() {
     " mov %[arpeggiatorOutput], %[arpeggiatorInput] \n\t" // set output to same as input
     : [arpeggiatorOutput] "=d" (arpeggiatorOutput) // output variables
     : [arpInc] "d" (arpInc), [arpMax] "d" (arpMax), [zero] "d" (zero), [arpeggiatorInput] "d" (arpeggiatorInput) // input variables
-    : "r16"
+    : 
   );
 
-  /*
-  asm volatile(
-    " addLoop: "
-    " mov r16, %[var1] \n\t"
-    " add r16, %[var1] \n\t"
-    " mov %[var2], r16 \n\t" 
-    : [var2] "=d" (var2) // output variables
-    : [var1] "d" (var1) // input variables
-    : "r16" // clobbers
-  );
-  */
-
-  delay(500);
-  Serial.print(arpeggiatorOutput);
-  delay(500);
-
+  // calls the bass line function, which uses the left, right, and bottom buttons to play arpeggiated notes
   bassLine(arpeggiatorOutput);
 
 }
 
-void melody(int tempo)
+// click joystick and move around to play notes
+void melody()
 {
-  delay(tempo);
+  int xValue = Esplora.readJoystickX(); // read the joystick's X axis
+  int yValue = Esplora.readJoystickY(); // read the joystick's Y axis
 
-  int xValue = Esplora.readJoystickX();        // read the joystick's X position
+  int joystickClick = Esplora.readJoystickSwitch(); // read the joystick button
 
-  int yValue = Esplora.readJoystickY();        // read the joystick's Y position
-
-  if (xValue != 0 || yValue != 0)
+  // if the joystick button is pressed, play notes based on the joystick position
+  if (joystickClick == 0)
   {
-    byte melodyNote = map(xValue, 0, yValue, 0, 13); 
-    // play the note corresponding to the slider's position:
-    Esplora.tone(note[melodyNote]);
+    Esplora.tone(note[xValue + yValue]);
   }
   else
   {
@@ -110,83 +93,51 @@ void melody(int tempo)
   }
 }
 
-// use slider to control pitch of bass line
+// hold left, right, and up buttons to play arpeggiators
 void bassLine(int arpeggiator)
 {
-  int slider = Esplora.readSlider();
-
-  if (slider > 0)
-  {
-    // use map() to map the slider's range to the
-    // range of notes you have:
-    byte thisNote = map((slider - 60) + arpeggiator, 0, 1023, 0, 255);
-    // play the note corresponding to the slider's position:
-    //Esplora.tone(note[thisNote]);
+  if (Esplora.readButton(SWITCH_DOWN) == LOW) {
+    Esplora.tone(note[arpeggiator]);
+  }
+  else if (Esplora.readButton(SWITCH_LEFT) == LOW) {
+    Esplora.tone(note[arpeggiator / 2]);
+  }
+  else if (Esplora.readButton(SWITCH_RIGHT) == LOW) {
+    Esplora.tone(note[arpeggiator * 2]);
   }
   else
   {
     Esplora.noTone();
   }
-
 }
 
+// hold up botton to play a drum loop
 void drumLoop()
 {
-  // click joystick to clear drum loop
-  float loopDuration = 4.0f;
-  
-  // mess w values later to make each drum diff
-  byte upNote = map(0, 0, 1023, 0, 13);
-  byte leftNote = map(50, 0, 1023, 0, 13);
-  byte rightNote = map(75, 0, 1023, 0, 13);
-  byte downNote = map(100, 0, 1023, 0, 13);
-
-  #pragma region AddDrumsToLoop
-  // press buttons to add drums to loop
   if (Esplora.readButton(SWITCH_UP) == LOW) {
-    Esplora.tone(note[upNote]);
+    Esplora.tone(note[2]);
   }
-  else if (Esplora.readButton(SWITCH_LEFT) == LOW) {
-    Esplora.tone(note[leftNote]);
+  else
+  {
+    Esplora.noTone();
   }
-  else if (Esplora.readButton(SWITCH_RIGHT) == LOW) {
-    Esplora.tone(note[rightNote]);
-  }
-  else if (Esplora.readButton(SWITCH_DOWN) == LOW) {
-    Esplora.tone(note[downNote]);
-  }
-  #pragma endregion AddDrumsToLoop
-
-  int joystickClick = Esplora.readJoystickSwitch();   
-
 }
 
-void getMic(int tempo)
+// output sound based on mic input
+void getMic()
 {
-  delay(tempo);
+  int loudness = Esplora.readMicrophone(); // read mic frequency
+  int soundDeadZone = 1; // set mic dead zone
 
-  // read mic sound
-  int loudness = Esplora.readMicrophone();
-  int soundDeadZone = 1;
-
-  // map the sound level to a brightness level for the LED:
-  int brightness = map(loudness, 0, 1023, 0, 255);
-  // write the brightness to the green LED:
-  Esplora.writeGreen(brightness);
-
-  // print the microphone levels and the LED levels (to see what's going on):
-  Serial.print("sound level: " + loudness);
-  Serial.print(" Green brightness: " + brightness); // frequency of mic sound
+  Esplora.writeGreen(loudness); // light up LED based on the mic frequency
 
   // play sound if mic reads a loud enough sound
   if (loudness > soundDeadZone)
   {
-    byte voiceBack = map(brightness, 0, 1023, 0, 13);
-    Esplora.tone(note[voiceBack]);
+    Esplora.tone(note[loudness]);
   }
   else
   {
     Esplora.noTone();
   }
-
 }
